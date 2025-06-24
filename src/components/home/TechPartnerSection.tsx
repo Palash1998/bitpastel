65060a7c8e80c36c97c5d9f5dafbc4bfd229df3b
Palash1998 <132887039+Palash1998@users.noreply.github.com>
2025-06-24 15:53:40 +0530

    TechPartnerSection.tsx

--- src/components/home/TechPartnerSection.tsx
+++ src/components/home/TechPartnerSection.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 import Button from '@/components/ui/Button'
 const TechPartnerSection = () => {
   return (
-      <section className="pt-16 lg:pt-16 ">
+      <section className="pt-12 lg:pt-16 ">
         <div className="mx-auto ">
           <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 ">
             <div
-              className="rounded-tr-[40px] rounded-br-[40px] overflow-hidden "
+              className="rounded-tr-[40px] rounded-br-[40px] overflow-hidden md:mr-0 mr-4 min-h-[15rem] md:min-h-full"
             >
               <Image
-                src="/images/tech_partner.jpg"
+                src="/images/person-client.png"
                 alt="Team collaboration"
                 width={711}
                 height={406}
@@ -18,15 +18,15 @@ const TechPartnerSection = () => {
                 // className="rounded-[40px] w-full h-auto"
               />
             </div>
-            <div className=" bg-primary-mint lg:p-12 md:p-12 p-6 rounded-tl-[40px] rounded-bl-[40px] content-center overflow-hidden">
+            <div className="md:ml-0 ml-4 bg-primary-mint lg:p-12 md:p-12 py-10 px-6 rounded-tl-[40px] rounded-bl-[40px] content-center overflow-hidden">
               {/* <div className="lg:w-1/2 bg-primary-mint rounded-[40px] p-12"> */}
-              <h2 className="text-4xl font-bold text-dark mb-8">
+              <h2 className=" md:leading-[45px] leading-[24px] font-source font-bold text-dark md:mb-8 mb-2 ">
                 <span className="font-semibold">Why we are the best </span>
                 <br />
                 <span className="font-black text-accent-green">Tech Partner</span>
                 <span className="font-semibold"> out there</span>
               </h2>
-              <div className="space-y-4">
+              <div className="md:space-y-4 space-y-2">
                 <div className='flex gap-4 align-middle'>
                   <Image
                     src="/images/img_materialsymbolscheckrounded_teal_300.svg"
@@ -34,7 +34,7 @@ const TechPartnerSection = () => {
                     width={24}
                     height={24}
                   />
-                  <p>Perfectly managed worry-free delivery</p>
+                  <p className='font-[400] font-roboto text-[rgba(33,37,41,1)]'>Perfectly managed worry-free delivery</p>
                 </div>
 
                <div className='flex gap-4 align-middle'>
@@ -44,7 +44,7 @@ const TechPartnerSection = () => {
                     width={24}
                     height={24}
                   />
-                  <p>Fast turn-around time</p>
+                  <p className='font-[400] font-roboto text-[rgba(33,37,41,1)]'>Fast turn-around time</p>
                 </div>
                  <div className='flex gap-4 align-middle'>
                   <Image
@@ -53,7 +53,7 @@ const TechPartnerSection = () => {
                     width={24}
                     height={24}
                   />
-                  <p>Multi-layered Quality Assurance</p>
+                  <p className='font-[400] font-roboto text-[rgba(33,37,41,1)]'>Multi-layered Quality Assurance</p>
                 </div>
                  <div className='flex gap-4 align-middle'>
                   <Image
@@ -62,7 +62,7 @@ const TechPartnerSection = () => {
                     width={24}
                     height={24}
                   />
-                  <p>Idea, Confidentiality & Data Protection</p>
+                  <p className='font-[400] font-roboto text-[rgba(33,37,41,1)]'>Idea, Confidentiality & Data Protection</p>
                 </div>
                <div className='flex gap-4 align-middle'>
                   <Image
@@ -71,7 +71,7 @@ const TechPartnerSection = () => {
                     width={24}
                     height={24}
                   />
-                  <p>30 days free maintenance</p>
+                  <p className='font-[400] font-roboto text-[rgba(33,37,41,1)]'>30 days free maintenance</p>
                 </div>
               </div>
             </div>
