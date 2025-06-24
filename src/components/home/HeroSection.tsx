0700188449f88006341ec2ce17ded02c9d9ec824
Palash1998 <132887039+Palash1998@users.noreply.github.com>
2025-06-24 15:50:51 +0530

    HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/ui/Button'
 const HeroSection = () => {
   return (
       <section
-        className="relative h-[686px] bg-cover bg-no-repeat md:bg-center bg-[position:63%_10%]"
+        className="relative md:h-[686px] h-[400px] bg-cover bg-center bg-no-repeat"
         style={{
           backgroundImage: `
       linear-gradient(270deg, rgba(0, 0, 0, 0) 49.05%, rgba(0, 0, 0, 0.5) 78.85%),
@@ -14,44 +14,50 @@ const HeroSection = () => {
       >
         <div className="relative z-10 container mx-auto px-4 h-full pt-[60px]">
           {/* Hero Content */}
-          <div className="h-full content-end md:content-center md:text-left text-center md:pb-0 pb-[40px] max-w-[35rem]">
-            <h1 className="md:text-[38px] text-[33px] text-white md:leading-[48px] leading-[40px] mb-6">
+          <div className="h-full content-center max-w-[35rem]">
+            <h1 className=" text-white md:leading-[48px] leading-[30px] mb-6">
               <span className="">Crafting </span>
-              <span className="font-bold">Digital Solutions</span>
-              <span className=""> for your business</span>
+              <span className="font-bold md:inline block">Digital Solutions</span>
+              <span className="block"> for your business</span>
             </h1>
-            <div className="text-[rgba(255,255,255,1)] w-fit md:size-auto md:mx-0 mx-auto">
-              <div className="flex gap-4 md:mb-1 mb-0 ">
+            <div className="text-[rgba(255,255,255,1)]">
+              <div className="flex md:gap-4 gap-2 mb-0">
                 <Image
                   src="/images/img_materialsymbolscheckrounded.svg"
                   alt="Check"
                   width={24}
                   height={24}
+                  className='w-[20px] md:w-auto'
                 />
-                <p className="text-[18px] md:leading-[48px] leading-[34px]">Shopify Plus & eCom Development</p>
+                <p className=" md:leading-[48px] leading-[24px] md:text-[16px] text-[12px]">Shopify Plus & eCom Development</p>
               </div>
-              <div className="flex gap-4 md:mb-1 mb-0 ">
+              <div className="flex md:gap-4 gap-2 mb-1">
                 <Image
                   src="/images/img_materialsymbolscheckrounded.svg"
+                  className='w-[20px] md:w-auto'
                   alt="Check"
                   width={24}
                   height={24}
                 />
-                <p className="text-[18px] md:leading-[48px] leading-[34px]">Web & Mobile App Development</p>
+                <p className=" md:leading-[48px] leading-[24px] md:text-[16px] text-[12px]">Web & Mobile App Development</p>
               </div>
-              <div className="flex gap-4 md:mb-1 mb-0 ">
+              <div className="flex md:gap-4 gap-2 mb-1">
                 <Image
                   src="/images/img_materialsymbolscheckrounded.svg"
+                  className='w-[20px] md:w-auto'
                   alt="Check"
                   width={24}
                   height={24}
                 />
-                <p className="text-[18px] md:leading-[48px] leading-[34px]">Marketing Solutions</p>
+                <p className="md:leading-[48px] leading-[24px] md:text-[16px] text-[12px]">Marketing Solutions</p>
               </div>
             </div>
-            <Button variant="secondary" size="lg" className="rounded-md px-8 mt-8 hover:text-white hover:bg-primary-teal transition-colors duration-200">
+            {/* <Button variant="secondary" size="lg" className=" btn leading-normal text-[rgba(255,255,255,1)] mt-8 hover:text-white hover:bg-primary-teal transition-colors duration-200 font-inter ">
               Lets Work Together
-            </Button>
+            </Button> */}
+            <button className=" btn leading-normal text-[rgba(30,30,30,1)] bg-[rgba(255,255,255,1)] mt-8 hover:text-white hover:bg-primary-teal transition-colors duration-200 font-inter">
+              Lets Work Together
+            </button>
           </div>
         </div>
       </section>
